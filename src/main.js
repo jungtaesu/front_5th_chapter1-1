1d21b381237e5e97a766aef5d1249ef81a980245
jungtaesu <xotndi0808@naver.com>
2025-03-23 09:07:05 +0900

    커밋 테스트

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const MainPage = () => `
   <div class="bg-gray-100 min-h-screen flex justify-center">
     <div class="max-w-md w-full">
       <header class="bg-blue-600 text-white p-4 sticky top-0">
-        <h1 class="text-2xl font-bold">항해플러스</h1>
+        <h1 class="text-2xl font-bold">항해1플러스</h1>
       </header>
 
       <nav class="bg-white shadow-md p-2 sticky top-14">
