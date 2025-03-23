bed694238e509ce22d7332a3ad418cde9dde13af
jungtaesu <xotndi0808@naver.com>
2025-03-23 21:43:57 +0900

    0323

--- src/main.js
+++ src/main.js
@@ -2,15 +2,53 @@ import postData from "./postData";
 
 const isLoggedIn = () => {
   const user = localStorage.getItem("user");
-  console.log("user", user);
+  // console.log("user", user);
   return user ? true : false;
 };
 
+function render() {
+  const path = window.location.pathname;
+  const root = document.getElementById("root");
+  if (!root) return;
+  console.log("root:", root);
+  if (path === "/") {
+    root.innerHTML = MainPage();
+  } else if (path === "/profile") {
+    //여길 들어간다면 user 정보 확인
+    if (!isLoggedIn()) {
+      history.pushState(null, "", "/login");
+      render();
+      return;
+    }
+    console.log(JSON.parse(localStorage.getItem("profile") || "{}"));
+    root.innerHTML = ProfilePage();
+  } else if (path === "/login") {
+    root.innerHTML = LoginPage();
+  } else {
+    root.innerHTML = ErrorPage();
+  }
+}
+
+const isHome = () => {
+  const isHome = window.location.pathname === "/";
+  return isHome ? true : false;
+};
+
+const isProfile = () => {
+  const isProfile = window.location.pathname === "/profile";
+
+  return isProfile ? true : false;
+};
+// const isLogin = window.location.pathname === "/login";
+
 const Nav = () => `
+<header class="bg-blue-600 text-white p-4 sticky top-0">
+<h1 class="text-2xl font-bold">항해플러스</h1>
+</header>
   <nav class="bg-white shadow-md p-2 sticky top-14">
     <ul class="flex justify-around">
-      <li><a href="/" class="text-blue-600">홈</a></li>
-      <li><a href="/profile" class="text-gray-600">프로필</a></li>
+      <li><a href="#" id="home" class=${isHome() ? "text-blue-600" : "text-gray-600"}>홈</a></li>
+      ${isLoggedIn() ? `<li><a href="#" id="profile" class=${isProfile() ? "text-blue-600" : "text-gray-600"}>프로필</a></li>` : ``}
       ${
         isLoggedIn()
           ? `<li><a href="#" id="logout" class="text-gray-600">로그아웃</a></li>`
@@ -23,9 +61,6 @@ const Nav = () => `
 const MainPage = () => `
   <div class="bg-gray-100 min-h-screen flex justify-center">
     <div class="max-w-md w-full">
-      <header class="bg-blue-600 text-white p-4 sticky top-0">
-        <h1 class="text-2xl font-bold">항해21플러스</h1>
-      </header>
       ${Nav()}
       <main class="p-4">
         <div class="mb-4 bg-white rounded-lg shadow p-4">
@@ -71,10 +106,10 @@ const LoginPage = () => `
       <h1 class="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>
       <form id="login-form123">
         <div class="mb-4">
-          <input id="test1234" type="text" placeholder="이메일 또는 전화번호" class="w-full p-2 border rounded">
+          <input id="userId" type="text" placeholder="사용자 이름" class="w-full p-2 border rounded">
         </div>
         <div class="mb-6">
-          <input id="test123" type="password" placeholder="비밀번호" class="w-full p-2 border rounded">
+          <input id="userPw" type="password" placeholder="비밀번호" class="w-full p-2 border rounded">
         </div>
         <button type="submit" class="w-full bg-blue-600 text-white p-2 rounded font-bold">로그인</button>
       </form>
@@ -89,77 +124,83 @@ const LoginPage = () => `
   </main>
 `;
 
-const ProfilePage = () => `
-  <div id="root">
-    <div class="bg-gray-100 min-h-screen flex justify-center">
-      <div class="max-w-md w-full">
+const ProfilePage = () => {
+  const profile = JSON.parse(localStorage.getItem("profile") || "{}");
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+  return `
+    <div id="root">
+      <div class="bg-gray-100 min-h-screen flex justify-center">
+        <div class="max-w-md w-full">
+        ${Nav()}
+          <main class="p-4">
+            <div class="bg-white p-8 rounded-lg shadow-md">
+              <h2 class="text-2xl font-bold text-center text-blue-600 mb-8">
+                내 프로필
+              </h2>
+              <form id="profile-submit">
+                <div class="mb-4">
+                  <label
+                    for="username"
+                    class="block text-gray-700 text-sm font-bold mb-2"
+                    >사용자 이름</label
+                  >
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="사용자 이름"
+                    value=${profile.username || user.id} 
+                    class="w-full p-2 border rounded"
+                  />
+                </div>
+                <div class="mb-4">
+                  <label
+                    for="email"
+                    class="block text-gray-700 text-sm font-bold mb-2"
+                    >이메일</label
+                  >
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value=${profile.email || "아무값"} 
+                    class="w-full p-2 border rounded"
+                  />
+                </div>
+                <div class="mb-6">
+                  <label
+                    for="bio"
+                    class="block text-gray-700 text-sm font-bold mb-2"
+                    >자기소개</label
+                  >
+                  <textarea
+                    id="bio"
+                    name="bio"
+                    rows="4"
+                    class="w-full p-2 border rounded"
+                  >
+${profile.bio || "아무값"}</textarea
+                  >
+                </div>
+                <button
+                  type="submit"
+                  class="w-full bg-blue-600 text-white p-2 rounded font-bold"
+                >
+                  프로필 업데이트
+                </button>
+              </form>
+            </div>
+          </main>
 
-        <main class="p-4">
-          <div class="bg-white p-8 rounded-lg shadow-md">
-            <h2 class="text-2xl font-bold text-center text-blue-600 mb-8">
-              내 프로필
-            </h2>
-            <form>
-              <div class="mb-4">
-                <label
-                  for="username"
-                  class="block text-gray-700 text-sm font-bold mb-2"
-                  >사용자 이름</label
-                >
-                <input
-                  type="text"
-                  id="username"
-                  name="username"
-                  value="홍길동"
-                  class="w-full p-2 border rounded"
-                />
-              </div>
-              <div class="mb-4">
-                <label
-                  for="email"
-                  class="block text-gray-700 text-sm font-bold mb-2"
-                  >이메일</label
-                >
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value="hong@example.com"
-                  class="w-full p-2 border rounded"
-                />
-              </div>
-              <div class="mb-6">
-                <label
-                  for="bio"
-                  class="block text-gray-700 text-sm font-bold mb-2"
-                  >자기소개</label
-                >
-                <textarea
-                  id="bio"
-                  name="bio"
-                  rows="4"
-                  class="w-full p-2 border rounded"
-                >
-안녕하세요, 항해플러스에서 열심히 공부하고 있는 홍길동입니다.</textarea
-                >
-              </div>
-              <button
-                type="submit"
-                class="w-full bg-blue-600 text-white p-2 rounded font-bold"
-              >
-                프로필 업데이트
-              </button>
-            </form>
-          </div>
-        </main>
-
-        <footer class="bg-gray-200 p-4 text-center">
-          <p>&copy; 2024 항해플러스. All rights reserved.</p>
-        </footer>
+          <footer class="bg-gray-200 p-4 text-center">
+            <p>&copy; 2024 항해플러스. All rights reserved.</p>
+          </footer>
+        </div>
       </div>
-    </div>
   </div>
-`;
+  `;
+};
+
 const ErrorPage = () => `
   <main class="bg-gray-100 flex items-center justify-center min-h-screen">
     <div class="bg-white p-8 rounded-lg shadow-md w-full text-center" style="max-width: 480px">
@@ -183,34 +224,16 @@ const ErrorPage = () => `
 //   ${ErrorPage()}
 // `;
 
-function render() {
-  const path = window.location.pathname;
-
-  if (path === "/") {
-    document.body.innerHTML = MainPage();
-  }
-  if (path === "/login") {
-    document.body.innerHTML = LoginPage();
-  }
-  if (path === "/profile") {
-    document.body.innerHTML = ProfilePage();
-  }
-  if (path === "/404") {
-    document.body.innerHTML = ErrorPage();
-  }
-}
-
 window.addEventListener("popstate", render);
 document.addEventListener("DOMContentLoaded", render);
 
 document.addEventListener("submit", (e) => {
-  console.log("e", e);
   if (e.target.id == "login-form123") {
     e.preventDefault();
     //없으면 새로고침됨.
     console.log("로그인 시도");
-    const id = document.getElementById("test1234");
-    const password = document.getElementById("test123");
+    const id = document.getElementById("userId");
+    const password = document.getElementById("userPw");
 
     localStorage.setItem(
       "user",
@@ -231,18 +254,56 @@ document.addEventListener("submit", (e) => {
       render();
     }
   }
-  console.log("localStorage", localStorage.getItem("user").id);
 });
 
 document.addEventListener("click", (e) => {
   if (e.target.id === "logout") {
     console.log("로그아웃");
     localStorage.removeItem("user");
-    history.go(0);
+    localStorage.removeItem("profile");
+    // history.go(0);
+    history.pushState(null, "", "/login");
+    render();
   }
   if (e.target.id === "login") {
     console.log("로그인 페이지 이동");
     history.pushState(null, "", "/login");
+    render();
+  }
+  if (e.target.id === "profile") {
+    console.log("profile 페이지 이동");
+    history.pushState(null, "", "/profile");
     console.log("window.location.pathname:", window.location.pathname);
+    render();
+  }
+  if (e.target.id === "home") {
+    console.log("home 페이지 이동");
+    history.pushState(null, "", "/");
+    console.log("window.location.pathname:", window.location.pathname);
+    render();
+  }
+});
+
+document.addEventListener("submit", (e) => {
+  console.log("e", e);
+  if (e.target.id == "profile-submit") {
+    e.preventDefault();
+    //없으면 새로고침됨.
+    console.log("프로필 시도");
+
+    const username = document.getElementById("username");
+    const email = document.getElementById("email");
+    const bio = document.getElementById("bio");
+
+    localStorage.setItem(
+      "profile",
+      JSON.stringify({
+        username: username.value,
+        email: email.value,
+        bio: bio.value,
+      }),
+    );
+
+    console.log("localStorage", localStorage.getItem("profile"));
   }
 });
